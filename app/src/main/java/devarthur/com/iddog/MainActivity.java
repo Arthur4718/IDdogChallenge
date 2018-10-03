54ce1fef08d5bdb5d1f19a9018b33c3ac100427f
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-03 12:28:16 -0300

    Check for valid email string.
    Added the onclickListener to the signInButton in the MainActiityClass, first check in the listener is for empty inputs. Added the isEmailValid method in the class to check for the "@" symbol in the e-mail string, returns true if the email is valid.

--- app/src/main/java/devarthur/com/iddog/MainActivity.java
+++ app/src/main/java/devarthur/com/iddog/MainActivity.java
@@ -2,9 +2,14 @@ package devarthur.com.iddog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +17,52 @@ public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView userEmailView;
     private Button signInButton;
 
+    //Constants
+    //Todo add any contast like api url , token and etc around here
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         userEmailView = (AutoCompleteTextView) findViewById(R.id.register_email);
+        //TODO Add an event to check for hover status. Black bg with white text when hovering, and White bg with black text when not
         signInButton = (Button) findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = userEmailView.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    //Todo show a dialog informing the user to add an email.
+                    Log.d("Dog", "Empty email input");
+                }else if(!isEmailValid(email)){
+                    //Todo show a dialog prompting the user to add a valid e-mail
+                    Log.d("Dog", "Invalid email string");
+
+                }else{
+                    //Todo call the POST method for the api
+                    Log.d("Dog", "Email valid");
+                }
+
+            }
+        });
+
+
+
     }
+
+    private boolean isEmailValid(String email){
+
+        return email.contains("@");
+    }
+
+    //TODO create a method to POST user email into the API
+
+
+    //TODO if the POST is successful, store the token locally.
 }
