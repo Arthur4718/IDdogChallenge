89e33dcf208cffae71a21ddc78f898a8bbeff0a3
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-03 11:34:50 -0300

    Sign in Button .
    Added the sign in button in the main activity xml. . Added the member variables for the Button and Textview, using findViewbyId to link member variables with the object view in the xml file. Added new string value called prompt_signIn.

--- app/src/main/java/devarthur/com/iddog/MainActivity.java
+++ app/src/main/java/devarthur/com/iddog/MainActivity.java
@@ -2,12 +2,23 @@ package devarthur.com.iddog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Ui References
+    private AutoCompleteTextView userEmailView;
+    private Button signInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userEmailView = (AutoCompleteTextView) findViewById(R.id.register_email);
+        signInButton = (Button) findViewById(R.id.signInButton);
     }
 }
