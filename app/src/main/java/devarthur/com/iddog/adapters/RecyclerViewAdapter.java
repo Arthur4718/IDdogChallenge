b91e464b10ad90dca05f629a26ac1173232cea86
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-05 16:55:02 -0300

    Recycler view Adapter
    Added an empty class to handle the adapter. Added methos to handle the adapter.

---
+++ app/src/main/java/devarthur/com/iddog/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,52 @@
+package devarthur.com.iddog.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Context;
+import java.util.List;
+
+
+import devarthur.com.iddog.model.DogDataModel;
+import devarthur.com.iddog.R;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.mViewHolder>{
+
+    private Context mContext;
+    private List<DogDataModel> mData;
+
+
+    public RecyclerViewAdapter(Context context, List<DogDataModel> data) {
+        mContext = context;
+        mData = data;
+    }
+
+    @Override
+    public mViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.dogimage_row_item,parent, false);
+        return new mViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(mViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class mViewHolder extends RecyclerView.ViewHolder {
+        public mViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
