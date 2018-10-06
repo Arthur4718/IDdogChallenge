7a09543222e67ff5812d129b9f8d317392331d92
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-05 23:52:28 -0300

    RecyclerView Adapter.
    Binding member variables to objects in the View.

--- app/src/main/java/devarthur/com/iddog/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/devarthur/com/iddog/adapters/RecyclerViewAdapter.java
@@ -1,10 +1,14 @@
 package devarthur.com.iddog.adapters;
 
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.List;
 
 
@@ -12,7 +16,7 @@ import devarthur.com.iddog.model.DogDataModel;
 import devarthur.com.iddog.R;
 
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.mViewHolder>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.mViewHolder> {
 
     private Context mContext;
     private List<DogDataModel> mData;
@@ -28,7 +32,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View view;
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        view = inflater.inflate(R.layout.dogimage_row_item,parent, false);
+        view = inflater.inflate(R.layout.dogimage_row_item, parent, false);
         return new mViewHolder(view);
     }
 
@@ -43,10 +47,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public static class mViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtImageDisplay;
+        ImageView img_data;
+
         public mViewHolder(View itemView) {
             super(itemView);
-        }
-    }
 
+            txtImageDisplay = itemView.findViewById(R.id.imageUrl);
+            img_data = itemView.findViewById(R.id.imgContentViewItem);
+        }
+
+
+    }
 
 }
